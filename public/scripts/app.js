ecc582bd6c94c4e4881da2f3a4e0ee72a0ea8b6a
Kaspern95 <34809965+Kaspern95@users.noreply.github.com>
2017-12-30 14:36:25 +0100

    Content and Setup done
    Cant get different pages to output different topics.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,45 @@
 "use strict";
 
-var template = React.createElement(
+var pageHead = React.createElement(
     "h1",
     null,
-    "Space"
+    "Space!"
 );
 
-//const buttonOne = <button onClick={buttonClick()} id="sun">SOLEN</button>;
+var appRoot = document.getElementById("app");
 
+ReactDOM.render(pageHead, appRoot);
+
+var pageContentSpace = React.createElement(
+    "div",
+    null,
+    React.createElement(
+        "h1",
+        null,
+        "What is space?"
+    ),
+    React.createElement(
+        "div",
+        null,
+        React.createElement(
+            "p",
+            null,
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra nisl dolor. Maecenas lacus metus, maximus sed risus sed, hendrerit aliquam elit. Pellentesque eget est et justo cursus bibendum. Mauris pharetra non nulla eget dapibus. Quisque est enim, dictum in quam quis, convallis suscipit sem. Morbi vitae diam vitae justo eleifend lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        ),
+        React.createElement("img", { src: "images/galaxy.jpg" }),
+        React.createElement(
+            "p",
+            null,
+            "Sed vitae risus lorem. In gravida fringilla leo, eu cursus nibh aliquet sed. Nullam fringilla id lacus pulvinar auctor. Maecenas pharetra mattis lectus. Pellentesque nec turpis quis urna mollis varius. Quisque id mauris sem. Sed malesuada lacinia ornare. Nunc eu ullamcorper neque, ac scelerisque justo. Proin volutpat tellus auctor dolor posuere bibendum eget in quam. Morbi leo lacus, hendrerit efficitur orci rhoncus, condimentum tincidunt libero. Mauris egestas tellus metus, ut molestie ipsum consectetur nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur tincidunt nulla id posuere dignissim. Donec vel metus turpis. Nulla tincidunt mattis imperdiet."
+        )
+    )
+);
+var appContentSpace = document.getElementById("content-space");
+ReactDOM.render(pageContentSpace, appContentSpace);
+
+ReactDOM.render(pageContentSun, appContentSun);
+
+//Button Functions
 function funcButtonOne() {
     document.location.replace("index.html");
 };
@@ -19,8 +51,3 @@ function funcButtonTwo() {
 function funcButtonThree() {
     document.location.replace("moon.html");
 };
-
-var appRoot = document.getElementById("app");
-var appB1 = document.getElementById("headerButtonOne");
-
-ReactDOM.render(template, appRoot);
--- src/app.js
+++ src/app.js
@@ -1,8 +1,35 @@
-const template = <h1>Space</h1>;
+const pageHead = <h1>Space!</h1>;
+
+const appRoot = document.getElementById("app");
+
+ReactDOM.render(pageHead, appRoot);
 
 
-//const buttonOne = <button onClick={buttonClick()} id="sun">SOLEN</button>;
+const pageContentSpace =(
+    <div>
+        <h1>
+            What is space?
+        </h1>
+        <div>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In viverra nisl dolor. Maecenas lacus metus, maximus sed risus sed, hendrerit aliquam elit. Pellentesque eget est et justo cursus bibendum. Mauris pharetra non nulla eget dapibus. Quisque est enim, dictum in quam quis, convallis suscipit sem. Morbi vitae diam vitae justo eleifend lobortis. Lorem ipsum dolor sit amet, consectetur adipiscing elit.    
+            </p>
+            <img src="images/galaxy.jpg"/>
+            <p>
+            Sed vitae risus lorem. In gravida fringilla leo, eu cursus nibh aliquet sed. Nullam fringilla id lacus pulvinar auctor. Maecenas pharetra mattis lectus. Pellentesque nec turpis quis urna mollis varius. Quisque id mauris sem. Sed malesuada lacinia ornare. Nunc eu ullamcorper neque, ac scelerisque justo. Proin volutpat tellus auctor dolor posuere bibendum eget in quam. Morbi leo lacus, hendrerit efficitur orci rhoncus, condimentum tincidunt libero. Mauris egestas tellus metus, ut molestie ipsum consectetur nec. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Curabitur tincidunt nulla id posuere dignissim. Donec vel metus turpis. Nulla tincidunt mattis imperdiet.
+            </p>
+        </div>
+    </div>
+);
+const appContentSpace = document.getElementById("content-space");
+ReactDOM.render(pageContentSpace, appContentSpace);
 
+
+
+ReactDOM.render(pageContentSun, appContentSun);
+
+
+//Button Functions
 function funcButtonOne(){
     document.location.replace("index.html");
 };
@@ -16,8 +43,4 @@ function funcButtonThree(){
 };
 
 
-const appRoot = document.getElementById("app");
-const appB1 = document.getElementById("headerButtonOne");
-
-ReactDOM.render(template, appRoot);
 
